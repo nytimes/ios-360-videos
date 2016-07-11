c4042b90bc036f8a1451a8bf6193afd1efac3cef
Thiago Pontes <github@thiago.me>
2016-07-11 14:47:30 -0400

    Initial Commit

---
+++ scenekittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scenekittest
+//
+//  Created by Thiago on 7/11/16.
+//  Copyright © 2016 The New York Times. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scenekittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scenekittest
+//
+//  Created by Thiago on 7/11/16.
+//  Copyright © 2016 The New York Times. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
