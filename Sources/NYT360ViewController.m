08f18f2ed05f60117299e497e2ffa95242653c1c
Jared Sinclair <jaredsinclair.rn@gmail.com>
2016-08-02 12:13:25 -0400

    Save work, unstable commit.

--- Sources/NYT360ViewController.m
+++ Sources/NYT360ViewController.m
@@ -12,18 +12,55 @@
 #import "NYT360CameraController.h"
 #import "NYT360PlayerScene.h"
 
+static inline CGRect NYT360ViewControllerSceneFrameForContainingBounds(CGRect containingBounds, CGSize underlyingSceneSize) {
+    
+    if (CGSizeEqualToSize(underlyingSceneSize, CGSizeZero)) {
+        return containingBounds;
+    }
+    
+    CGSize containingSize = containingBounds.size;
+    CGFloat heightRatio = containingSize.height / underlyingSceneSize.height;
+    CGFloat widthRatio = containingSize.width / underlyingSceneSize.width;
+    CGSize targetSize;
+    if (heightRatio > widthRatio) {
+        targetSize = CGSizeMake(underlyingSceneSize.width * heightRatio, underlyingSceneSize.height * heightRatio);
+    } else {
+        targetSize = CGSizeMake(underlyingSceneSize.width * widthRatio, underlyingSceneSize.height * widthRatio);
+    }
+    
+    CGRect targetFrame = CGRectZero;
+    targetFrame.size = targetSize;
+    targetFrame.origin.x = (containingBounds.size.width - targetSize.width) / 2.0;
+    targetFrame.origin.y = (containingBounds.size.height - targetSize.height) / 2.0;
+    
+    return targetFrame;
+}
+
+static inline CGRect NYT360ViewControllerSceneBoundsForScreenBounds(CGRect screenBounds) {
+    CGFloat max = MAX(screenBounds.size.width, screenBounds.size.height);
+    CGFloat min = MIN(screenBounds.size.width, screenBounds.size.height);
+    return CGRectMake(0, 0, max, min);
+}
+
 @interface NYT360ViewController ()
 
-@property (nonatomic) NYT360PlayerScene *playerScene;
-@property (nonatomic) NYT360CameraController *cameraController;
+@property (nonatomic, readonly) CGSize underlyingSceneSize;
+@property (nonatomic, readonly) SCNView *sceneView;
+@property (nonatomic, readonly) NYT360PlayerScene *playerScene;
+@property (nonatomic, readonly) NYT360CameraController *cameraController;
 
 @end
 
 @implementation NYT360ViewController
 
-- (id)initWithAVPlayer:(AVPlayer *)player {
+- (instancetype)initWithAVPlayer:(AVPlayer *)player {
     self = [super init];
     if (self) {
+        CGRect screenBounds = [[UIScreen mainScreen] bounds];
+        CGRect initialSceneFrame = NYT360ViewControllerSceneBoundsForScreenBounds(screenBounds);
+        _underlyingSceneSize = initialSceneFrame.size;
+        _sceneView = [[SCNView alloc] initWithFrame:initialSceneFrame];
+        _cameraController = [[NYT360CameraController alloc] initWithView:_sceneView];
         _playerScene = [[NYT360PlayerScene alloc] initWithAVPlayer:player];
     }
     return self;
@@ -40,14 +77,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    SCNView *view = (SCNView *)self.view;
-    view.backgroundColor = [UIColor greenColor];
-    view.delegate = self;
+    self.view.backgroundColor = [UIColor blueColor];
+    self.view.opaque = YES;
     
-    [self.playerScene bindToView:view];
+    // self.sceneView.showsStatistics = YES;
+    self.sceneView.autoresizingMask = UIViewAutoresizingNone;
+    self.sceneView.backgroundColor = [UIColor blackColor];
+    self.sceneView.opaque = YES;
+    self.sceneView.delegate = self;
+    [self.view addSubview:self.sceneView];
     
-    self.cameraController = [[NYT360CameraController alloc] initWithView:view];
+    [self.playerScene bindToView:self.sceneView];
+    
     [self adjustCameraFOV];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.sceneView.frame = NYT360ViewControllerSceneFrameForContainingBounds(self.view.bounds, self.underlyingSceneSize);
 }
 
 - (void)viewWillAppear:(BOOL)animated {
