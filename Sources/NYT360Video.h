d94a500789d9c723e8e529d3f3bbe42b7efbd234
Jared Sinclair <jaredsinclair.rn@gmail.com>
2016-08-04 11:28:40 -0400

    Rename Three60_Player_iOS to NYT360Video.

--- Sources/NYT360Video.h
+++ Sources/NYT360Video.h
@@ -14,7 +14,7 @@ FOUNDATION_EXPORT double NYT360VideoVersionNumber;
 //! Project version string for 360 Player.
 FOUNDATION_EXPORT const unsigned char NYT360VideoVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Three60_Player_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <NYT360Video/PublicHeader.h>
 
 #import <NYT360Video/NYT360ViewController.h>
 #import <NYT360Video/NYT360DataTypes.h>
