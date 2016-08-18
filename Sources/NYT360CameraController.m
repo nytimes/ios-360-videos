b4d417e1da2b76e1d40432e31b53b1d7e06d3a7e
Jared Sinclair <jaredsinclair.rn@gmail.com>
2016-08-18 13:35:23 -0500

    Fix error with renamed ivar.

--- Sources/NYT360CameraController.m
+++ Sources/NYT360CameraController.m
@@ -73,7 +73,7 @@ static inline CGPoint subtractPoints(CGPoint a, CGPoint b) {
 
 - (double)cameraAngleDirection {
     float x = 0, y = 0, z = -1;
-    SCNMatrix4 worldMatrix = _camera.worldTransform;
+    SCNMatrix4 worldMatrix = self.pointOfView.worldTransform;
 
     float qx = worldMatrix.m11 * x + worldMatrix.m12 * y + worldMatrix.m13 * z + worldMatrix.m14;
     float qz = worldMatrix.m31 * x + worldMatrix.m32 * y + worldMatrix.m33 * z + worldMatrix.m34;
